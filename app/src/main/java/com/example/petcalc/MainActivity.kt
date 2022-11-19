741db46904bcb16f0d3230ea794a1b720c88b0a9
qsonic <bashkin415@gmail.com>
2022-11-19 15:56:13 +0300

    started refactoring

--- app/src/main/java/com/example/petcalc/MainActivity.kt
+++ app/src/main/java/com/example/petcalc/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.petcalc
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,57 +11,59 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var but_calc: Button
-    lateinit var neck: EditText
-    lateinit var body: EditText
-    lateinit var height: EditText
-    lateinit var textsize: TextView
-    lateinit var size_net: TextView
-    lateinit var Main_text: TextView
+    private lateinit var calcButton: Button
+    private lateinit var neckET: EditText
+    private lateinit var bodyET: EditText
+    private lateinit var heightET: EditText
+    private lateinit var sizeTextView: TextView
+    private lateinit var sizeValueTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        but_calc = findViewById(R.id.Calculate_but)
-        textsize = findViewById(R.id.Size_pet)
-        size_net = findViewById(R.id.Sizes)
-        neck = findViewById(R.id.Edit_Neck)
-        body = findViewById(R.id.Edit_Body)
-        height = findViewById(R.id.Edit_Height)
 
-        but_calc.setOnClickListener {
+        calcButton = findViewById(R.id.calcuclateBtn)
+        sizeTextView = findViewById(R.id.sizeText)
+        sizeValueTextView = findViewById(R.id.sizeValueText)
+        neckET = findViewById(R.id.neckEditText)
+        bodyET = findViewById(R.id.bodyEditText)
+        heightET = findViewById(R.id.heightEditText)
 
-            size_net.text = set_calculate()
-
-        }
+        calcButton.setOnClickListener { onCalculateClicked() }
     }
 
-    @SuppressLint("ShowToast")
-    private fun set_calculate(): String? {
-        val n = neck.text.toString()
-        val b = body.text.toString()
-        val h = height.text.toString()
+    private fun onCalculateClicked() {
+        val neckSize = neckET.text.toString()
+        val bodySize = bodyET.text.toString()
+        val height = heightET.text.toString()
 
-        if (n.trim().isNotEmpty() && b.trim().isNotEmpty() && h.trim().isNotEmpty()) {
-            textsize.text = "Size your pet" // С visibility не разобрался, поэтому костыль
-            when {
-                b.toInt() in 28..36 || n.toInt() in 18..22 && h.toInt() >= 20 -> return "XS"
-                b.toInt() in 36..40 || n.toInt() in 22..26 && h.toInt() >= 26 -> return "S"
-                b.toInt() in 40..46 || n.toInt() in 26..30 && h.toInt() >= 30 -> return "M"
-                b.toInt() in 46..50 || n.toInt() in 30..34 && h.toInt() >= 36 -> return "L"
-                b.toInt() >= 50 || n.toInt() >= 34 && h.toInt() >= 40 -> return "XL"
-                else -> return "Не могу вычислить"
-            }
+        if (neckSize.trim().isNotEmpty() && bodySize.trim().isNotEmpty() && height.trim()
+                .isNotEmpty()
+        ) {
+            sizeTextView.visibility = View.VISIBLE
+            sizeValueTextView.visibility = View.VISIBLE
+            sizeValueTextView.text = getSizeValue(
+                neckSize = neckSize.toInt(),
+                bodySize = bodySize.toInt(),
+                height = height.toInt()
+            )
         } else {
-            val text_alarm = "Введите размеры!"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(applicationContext, text_alarm, duration)
-            toast.setGravity(Gravity.CENTER, 0, 0)
-            toast.show()
-            return null
+            Toast.makeText(applicationContext, R.string.enter_sizes, Toast.LENGTH_SHORT).show()
         }
-
     }
 
-
+    private fun getSizeValue(
+        neckSize: Int,
+        bodySize: Int,
+        height: Int
+    ): String {
+        return when {
+            bodySize in 28..36 || neckSize in 18..22 && height >= 20 -> "XS"
+            bodySize in 36..40 || neckSize in 22..26 && height >= 26 -> "S"
+            bodySize in 40..46 || neckSize in 26..30 && height >= 30 -> "M"
+            bodySize in 46..50 || neckSize in 30..34 && height >= 36 -> "L"
+            bodySize >= 50 || neckSize >= 34 && height >= 40 -> "XL"
+            else -> "Не могу вычислить"
+        }
+    }
 }
